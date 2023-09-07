1464ec6738abacfbaa604ca653fb0c46f14e1db9
mishba-ai <mishbabagban@gmail.com>
2023-09-07 16:24:49 +0530

    setup the react and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <h1 className="text-6xl underline text-purple-600 bg-purple-300">salam!</h1>
+}
+
+export default App;
